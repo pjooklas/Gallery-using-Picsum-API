f7d5e7408e2c24a9ed69a6a394b80c83e131e413
pjooklas <pjooklas@users.noreply.github.com>
2022-04-24 19:13:33 +0300

    Show image on right side

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-import { makeRequest } from "./utils.js";
+const leftPane = document.querySelector('.left-side');
+const rightPane = document.querySelector('.right-side');
+const baseURL = 'https://picsum.photos/id/';
 
-makeRequest();
+fetch('https://picsum.photos/v2/list')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            const aTag = document.createElement('a');
+            const img = document.createElement('img');
+            img.setAttribute('src', baseURL + element.id + '/200/200');
+            img.setAttribute('id', element.id);
+            aTag.setAttribute('class', 'link');
+            aTag.setAttribute('href', element.download_url);
+            aTag.setAttribute('target', "right-side");
+            aTag.appendChild(img);
+            leftPane.appendChild(aTag);
+
+            img.addEventListener('click', () => {
+
+                console.log('paklikinai' + element.id);
+            })
+        });
+    });
